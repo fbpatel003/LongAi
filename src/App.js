b6f6e49c492a21ef4b53317e0b42845a7f6c1a50
Fenil Patel <102594553+fbpatel003@users.noreply.github.com>
2022-12-16 12:24:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProjectPage from "./Components/ProjectPage";
 import DashBoard from "./Components/DashBoard";
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ProjectPage />}>
-        <Route path="/MyProject" element={<ProjectPage />} />
-        <Route path="/Dashboard" element={<DashBoard />} />
+          <Route path="/MyProject" element={<ProjectPage />} />
+          <Route path="/Dashboard" element={<DashBoard />} />
         </Route>
       </Routes>
     </BrowserRouter>
